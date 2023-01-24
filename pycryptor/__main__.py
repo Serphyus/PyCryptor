d96b6d2e6a067aa934d325efe2ddb67a5711afc3
Sigurd-Pettersen <73425444+Sigurd-Pettersen@users.noreply.github.com>
2023-01-24 23:24:38 +0100

    added help and description for argument parsing

--- pycryptor/__main__.py
+++ pycryptor/__main__.py
@@ -6,13 +6,13 @@ from Crypto.Cipher import AES
 
 
 def main() -> None:
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description="cli tool for encrypting and decrypting files")
 
-	parser.add_argument("paths", nargs="*")
-	parser.add_argument("-e", "--encrypt", action="store_true")
-	parser.add_argument("-d", "--decrypt", action="store_true")
-	parser.add_argument("-k", "--key", action="store")
-	parser.add_argument("-b", "--buffer_size", action="store", type=int, default=4096)
+	parser.add_argument("paths", nargs="*", help="filepaths to encrypt/decrypt")
+	parser.add_argument("-e", "--encrypt", action="store_true", help="encrypt mode")
+	parser.add_argument("-d", "--decrypt", action="store_true", help="decrypt mode")
+	parser.add_argument("-k", "--key", action="store", help="password to use for encrypting/decrypting")
+	parser.add_argument("-b", "--buffer_size", action="store", type=int, default=4096, help="buffer size which must be a product of 16")
 
 	args = parser.parse_args()
 
